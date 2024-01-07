b6abe8315ddcb3deb79cf85bb651acb77acd0516
PalZol <p.z.palasti@gmail.com>
2024-01-07 15:13:55 +0100

    chnaged to windows

--- user_login.py
+++ user_login.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome()
